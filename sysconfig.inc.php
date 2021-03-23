d02047fea2ac67249d9d3249bf1fe54ac9979113
imran16bs <imran.hossain@brainstation-23.com>
2021-03-23 11:25:09 +0600

    SysConfig Email Server and port restored as before

--- sysconfig.inc.php
+++ sysconfig.inc.php
@@ -463,8 +463,8 @@ if ($sysconf['captcha']['member']['enable']) {
  * Mailing Settings
  */
 $sysconf['mail']['enable'] = true;
-$sysconf['mail']['server'] = 'smtp.gmail.com'; // SMTP server
-$sysconf['mail']['server_port'] = 587; // the SMTP port
+$sysconf['mail']['server'] = 'ssl://smtp.gmail.com:465'; // SMTP server
+$sysconf['mail']['server_port'] = 465; // the SMTP port
 $sysconf['mail']['auth_enable'] = true; // enable SMTP authentication
 $sysconf['mail']['auth_username'] = 'bard.erp.contact@gmail.com'; // SMTP account username
 $sysconf['mail']['auth_password'] = 'bs_23@ErpBard'; // SMTP account password
